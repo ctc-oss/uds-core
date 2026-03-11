7dfe417b56559fc6cc05f02adf43a15d66459820
Shawn J. Dorsch <dorschs@ctc.com>
2026-03-11 11:16:50 -0400

    round of updates to get grafana working from grafana.admin.uds.dev

--- src/pepr/operator/controllers/keycloak/clients/client-credentials.ts
+++ src/pepr/operator/controllers/keycloak/clients/client-credentials.ts
@@ -5,7 +5,7 @@
 
 import { fetch, K8s, kind } from "pepr";
 import { Client } from "../types";
-import { baseUrl, log, throwErrorIfNeeded } from "./common";
+import { getBaseUrl, log, throwErrorIfNeeded } from "./common";
 
 export interface ClientWithId extends Client {
   id: string;
@@ -19,8 +19,13 @@ export function parseKeycloakToken(token: string) {
   return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString()) as KeycloakToken;
 }
 
-const clientsAdminUrl = `${baseUrl}/cge/sso/admin/realms/uds/clients`;
-const clientCredentialsUrl = `${baseUrl}/cge/sso/realms/uds/protocol/openid-connect/token`;
+function getClientsAdminUrl() {
+  return `${getBaseUrl()}/admin/realms/uds/clients`;
+}
+
+function getClientCredentialsUrl() {
+  return `${getBaseUrl()}/realms/uds/protocol/openid-connect/token`;
+}
 const SECRET_NAMESPACE = "keycloak";
 const SECRET_NAME = "keycloak-client-secrets";
 const UDS_OPERATOR_CLIENT_ID = "uds-operator";
@@ -64,7 +69,7 @@ export async function credentialsGetAccessToken() {
   params.append("client_id", UDS_OPERATOR_CLIENT_ID);
   params.append("client_secret", clientSecret);
 
-  const response = await fetch<KeycloakAccessTokenResponse>(clientCredentialsUrl, {
+  const response = await fetch<KeycloakAccessTokenResponse>(getClientCredentialsUrl(), {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: params.toString(),
@@ -87,7 +92,7 @@ export async function credentialsCreateOrUpdate(client: Partial<Client>) {
 export async function credentialsCreate(client: Partial<Client>) {
   log.info(`credentialsCreate: creating client ${JSON.stringify(client)}`);
   const token = await credentialsGetAccessToken();
-  const response = await fetch(clientsAdminUrl, {
+  const response = await fetch(getClientsAdminUrl(), {
     method: "POST",
     headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
     body: JSON.stringify(client),
@@ -101,7 +106,7 @@ export async function credentialsCreate(client: Partial<Client>) {
 export async function credentialsGet(client: Partial<Client>) {
   log.info(`credentialsGet: retrieving client ${JSON.stringify(client)}`);
   const token = await credentialsGetAccessToken();
-  const url = `${clientsAdminUrl}?clientId=${encodeURIComponent(client.clientId!)}`;
+  const url = `${getClientsAdminUrl()}?clientId=${encodeURIComponent(client.clientId!)}`;
   // There's no Client GET REST endpoint that obtains a client based on client_id (the logical client name, like uds-operator).
   // All Admin REST endpoints for client operator on the database Client ID, which is a UUID. The only interface that allows to
   // obtain the Client using the client_id is the collection interface which returns a singular collection with
@@ -123,7 +128,7 @@ export async function credentialsUpdate(client: Partial<Client>) {
   if (!existing || !existing.id) {
     throw new Error(`Failed to retrieve existing client, ${client.clientId}`);
   }
-  const url = `${clientsAdminUrl}/${encodeURIComponent(existing.id)}`;
+  const url = `${getClientsAdminUrl()}/${encodeURIComponent(existing.id)}`;
   const response = await fetch(url, {
     method: "PUT",
     headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
@@ -142,7 +147,7 @@ export async function credentialsDelete(client: Partial<Client>) {
   if (!existing || !existing.id) {
     throw new Error(`Failed to retrieve existing client, ${client.clientId}`);
   }
-  const url = `${clientsAdminUrl}/${encodeURIComponent(existing.id)}`;
+  const url = `${getClientsAdminUrl()}/${encodeURIComponent(existing.id)}`;
   const response = await fetch(url, {
     method: "DELETE",
     headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
